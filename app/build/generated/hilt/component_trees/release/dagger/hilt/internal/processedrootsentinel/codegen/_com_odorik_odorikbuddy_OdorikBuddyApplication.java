06f8d852b0b8fb7cdf7e570f9a2224cd6f824fdf
Sentello <sentello@segfault.sk>
2025-11-05 21:46:15 +0200

    Remove build artifacts from version control

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_odorik_odorikbuddy_OdorikBuddyApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.odorik.odorikbuddy.OdorikBuddyApplication"
-)
-public final class _com_odorik_odorikbuddy_OdorikBuddyApplication {
-}
